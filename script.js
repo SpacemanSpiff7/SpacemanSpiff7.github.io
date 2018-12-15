d24cbbe465c990102ca72376ebdfe9ef4acac302
SpacemanSpiff7 <simone.longo@gmail.com>
2018-12-14 18:24:22 -0700

    Updated script.js

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-function changeFont()
-{
-  var strings = [", I", " am", " the", " mountain"];
-  var i = 0;
-  var title  = document.queryselector("#mainTitle");
-  title.innerHTML += strings[i];
-  if (i < strings.length)
-    {
-      i++;
-    }
-}
-   var parameters = {
-  target: '#myFunction',
-  data: [{
-    fn: 'sin(x)', 
-    color: 'black'
- }       
-        ],
-  grid: true,
-  yAxis: {domain: [-1.2, 1.2]},
-  xAxis: {domain: [-6, 6]}
-};
+var textElem = document.getElementById("animate")
 
-functionPlot(parameters);
+var on = false;
+function toggle(){
+	if (on){
+		textElem.innerHTML = "(¬‿¬)"	
+	}
+	else { 
+		textElem.innerHTML = "(◕‿◕)"
+	}
+	on = !on
+}
+
+setInterval(toggle, 1500)
